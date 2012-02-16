0dcac1c9a81adb439d56e687d0932d526373fde8
Mario Geiger <geiger.mario@gmail.com>
2012-02-16 12:30:17 +0100

    Comments for cmdlineparser

--- cmdlineparser.h
+++ cmdlineparser.h
@@ -9,7 +9,11 @@ class CmdLineParser
 public:
     CmdLineParser(const QStringList &arguments);
 
+    // cherche "--key=*", s'il n'existe pas retourne defaultValue
+    // si defaultValue est une nombre et que la valeur n'est pas convertible, retourne defaultValue
     QVariant get(const QByteArray &key, const QVariant &defaultValue);
+
+    // cherche dans les agruments : "--key" ou "--key=*", s'il trouve retourne true.
     bool isSet(const QByteArray &key);
 
 private:
